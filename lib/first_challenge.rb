ed68c5dad2ecc9f8c996e0f90ae34f3a1a7f596a
Eric <eric.m.lombardo@gmail.com>
2020-11-20 10:30:46 -0500

    commit tree 02ad351eb5981550129288af053712a7b4bb93e4

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,9 @@
 def first_challenge
-   epic_tragedy = {}
+   # set hash eq 2 :montague and :capulet
+   epic_tragedy = {
+      :montague => {},
+      :capulet => {}
+   }
 end
+
+# numbers as strings
